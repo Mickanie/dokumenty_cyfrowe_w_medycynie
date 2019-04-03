6b3424a575c6073214fcc21a0cfd3d8029406b45
Mickanie <michalina.kaniewska@gmail.com>
2019-04-03 22:57:49 +0200

    Updates

---
+++ precache-manifest.0468e6dc05003970ee259547956d13c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/dokumenty_cyfrowe_w_medycynie/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "434f13d307f07ab918bf",
+    "url": "/dokumenty_cyfrowe_w_medycynie/static/js/runtime~main.68604f27.js"
+  },
+  {
+    "revision": "38a800b02b47c43b7a23",
+    "url": "/dokumenty_cyfrowe_w_medycynie/static/js/main.01617193.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/dokumenty_cyfrowe_w_medycynie/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "38a800b02b47c43b7a23",
+    "url": "/dokumenty_cyfrowe_w_medycynie/static/css/main.6a85f1a0.chunk.css"
+  },
+  {
+    "revision": "0424084d89097f19c6e7af47197c22ab",
+    "url": "/dokumenty_cyfrowe_w_medycynie/index.html"
+  }
+];
